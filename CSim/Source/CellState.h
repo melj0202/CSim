923cc8db5a963888ee5763178af2110b0e755349
melj0202 <61242707+melj0202@users.noreply.github.com>
2024-09-22 19:18:49 -0400

    Update CellState.h
    Added some TODO comments

--- CSim/Source/CellState.h
+++ CSim/Source/CellState.h
@@ -7,6 +7,10 @@
 		This class defines the main loop behavior. Inherit and override the class functions to define main loop behavior...
 
 		DO NOT EVER CALL THE BASECLASS DIRECTLY
+
+  		TODO: Add extra data to this class so that a state may switch back to a previous state if needed.
+
+    		This would allow for the load/save states to go back into edit mode when done.
 */
 class CellState {
 public: 
